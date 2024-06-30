c6eda6f661a75c9fb6fa059920339a1771241b33
JLP <jlpoltrack@gmail.com>
2024-06-29 20:31:22 -0400

    keymap rules

--- keyboards/converter/ibmpc_usb/keymaps/vial_VERY_INSECURE/rules.mk
+++ keyboards/converter/ibmpc_usb/keymaps/vial_VERY_INSECURE/rules.mk
@@ -6,3 +6,9 @@ MOUSEKEY_ENABLE = no
 QMK_SETTINGS = no
 VIAL_INSECURE = yes
 OPT_DEFS += -DVIAL_VERY_INSECURE
+
+
+# Extra options to save space
+# TAP_DANCE_ENABLE = no
+# COMBO_ENABLE = no
+# KEY_OVERRIDE_ENABLE = no
